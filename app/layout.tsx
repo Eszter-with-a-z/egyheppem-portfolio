2f4cf51c480d75f7838050e0a12921823ea795de
eszter-with-a-z <eszter.with.a.z@gmail.com>
2025-11-03 20:59:21 +0100

    Add custom icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,15 @@ import { Navigation } from "@/components/navigation"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "egyheppem - Kosiba Zsófia Photography",
-  description: "Photography portfolio by Kosiba Zsófia",
-    generator: 'v0.app'
+  title: "egyheppem | Kosiba Zsófia ",
+  description: "Kosiba Zsófia fényképész portfóliója",
+    generator: 'v0.app',
+  icons: {
+    /* Copy the URL from the Cloudinary image you want to use
+        Add `/c_fill,h_64,w_64,g_face,r_max,f_png,b_transparent` after upload/, before v17xxx...
+    */
+    icon: "https://res.cloudinary.com/da5o0fitn/image/upload/c_fill,h_64,w_64,g_face,r_max,f_png,b_transparent/v1762194768/portfolio/profile-pictures/profile-picture_13.jpg",
+  }
 }
 
 export default function RootLayout({
