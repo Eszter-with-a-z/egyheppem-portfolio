aae60f8b70197170ea0208b74778e8a2bdae5429
eszter-with-a-z <eszter.with.a.z@gmail.com>
2025-11-04 11:26:54 +0100

    Refactor Best Picutres for more personalizability

--- lib/data-best-pictures.ts
+++ lib/data-best-pictures.ts
@@ -1,21 +1,47 @@
 export interface BestPicture {
-  id: string
-  title: string
-  image: string
-  projectId: string
+  id: string // number it manually
+  title: string // What you want to show up on the slideshow
+  image: string // `${CLOUDINARY_BASE}/project-name/image-name.jpg`
+  projectId: string // MUST BE THE SAME as lib/data-projects > Project > "id: string"
 }
 
 const CLOUDINARY_BASE = 
-"https://res.cloudinary.com/da5o0fitn/image/upload/q_auto,f_auto/v1761992999/portfolio/best-pictures"
+"https://res.cloudinary.com/da5o0fitn/image/upload/q_auto,f_auto/v1761992999/portfolio/projects"
 
-const TOTAL_IMAGES = 13
 
-export const bestPictures: BestPicture[] = Array.from({ length: TOTAL_IMAGES }, (_, i) => {
-  const id = (i + 1).toString()
-  return {
-    id,
-    title: `Kedvenc képeim válogatás, ${id}. kép`,
-    image: `${CLOUDINARY_BASE}/best-picture_${id}.jpg`,
-    projectId:`best-picture-${id}`
-  }
-})
+
+export const bestPictures: BestPicture[] = [
+  /* bestPicture template
+  {
+    id:'1',
+    title: `Kedvenc képeim válogatás,kép`,
+    image: `${CLOUDINARY_BASE}/project-name/image-name.jpg`,
+    projectId:`project-name`
+  },
+  END of bestPicture template*/
+  {
+    id:'1',
+    title: `Kedvenc képeim válogatás,kép`,
+    image: `${CLOUDINARY_BASE}/bankito/nap-3/nap-3_137.jpg`,
+    projectId:`bankito/nap-3`
+  },
+  {
+    id:'2',
+    title: `Kedvenc képeim válogatás,kép`,
+    image: `${CLOUDINARY_BASE}/bankito/nap-2/nap-2_77.jpg`,
+    projectId:`bankito/nap-2`    
+  },
+  {
+    id:'3',
+    title: `Kedvenc képeim válogatás,kép`,
+    image: `${CLOUDINARY_BASE}/bankito/nap-1/nap-1_120.jpg`,
+    projectId:`bankito/nap-1`
+    
+  },
+    {
+    id:'4',
+    title: `Kedvenc képeim válogatás,kép`,
+    image: `${CLOUDINARY_BASE}/bankito/nap-0/nap-0-18.jpg`,
+    projectId:`bankito/nap-0`
+  },
+]
